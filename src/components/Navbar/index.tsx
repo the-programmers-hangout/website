8ac21f29176659a7eff7d00c5b1eba3bbd9b456f
Xetera <mythologyali@gmail.com>
2019-07-26 15:53:24 -0700

    basic style placeholders

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,44 +1,30 @@
 
 import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import React from "react";
 import icon from "./icon.png";
-import { HeaderWrapper } from "./styles";
+import { NavbarWrapper, NavbarItems } from "./styles";
 
-const Navbar = ({ siteTitle }) => (
-  <HeaderWrapper>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-        {/* <img src={icon} style={{ width: "70px" }} /> */}
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h1>
-      </div>
-    </div>
-
-  </HeaderWrapper>
+const Navbar = ({ siteTitle = "" }) => (
+  <NavbarWrapper>
+    <NavbarItems>
+      {/* <img src={icon} style={{ width: "70px" }} /> */}
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </NavbarItems>
+    <NavbarItems>
+      <button>discord link here</button>
+      <button>reddit (maybe?) link here</button>
+    </NavbarItems>
+  </NavbarWrapper>
 )
-
-Navbar.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Navbar.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Navbar;
--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
