4ff5d0c3777e331d450809f4d863bf62c8608590
Jean-Philippe Sirois <jp@veksen.net>
2019-07-27 04:12:17 -0400

    feat: bootstrap docs layout & sidebar

--- src/templates/languagePost.tsx
+++ src/templates/languagePost.tsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { graphql } from "gatsby";
-import SEO from "../components/SEO";
-import Layout from "../components/Layout";
-import { Sidebar } from "../components/Docs";
+import React from "react"
+import { graphql } from "gatsby"
+import SEO from "../components/SEO"
+import DocsLayout from "../components/DocsLayout"
 
 // @todo maybe find alternative type for data
 const LanguagePost = ({ data }: any) => {
-  const { html, frontmatter } = data.file.post;
+  const { html, frontmatter } = data.file.post
   console.log(data)
   return (
-    <Layout>
+    <DocsLayout>
       <SEO title={frontmatter.title} />
-      <Sidebar />
       <div dangerouslySetInnerHTML={{ __html: html }} />
-    </Layout >
+    </DocsLayout>
   )
 }
 
-export default LanguagePost;
+export default LanguagePost
 
 export const query = graphql`
   query LanguagePost($file: String!) {
-    file(relativePath: {eq: $file }) {
+    file(relativePath: { eq: $file }) {
       post: childMarkdownRemark {
         html
         frontmatter {
