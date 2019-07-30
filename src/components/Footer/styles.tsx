fc0683a40754891788198c296a787c3001573dcf
Thomas Ibarra <hello@imake.software>
2019-07-30 17:50:33 -0500

    fix(footer): adjust width for center panel

--- src/components/Footer/styles.tsx
+++ src/components/Footer/styles.tsx
@@ -2,6 +2,9 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 
 export const FooterWrapper = styled.footer`
+  max-width: 840px;
+  margin: auto auto;
+
   & a {
     display: inline;
   }
