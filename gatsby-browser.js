569146446edcfc516659a578e6abef53c5bed609
Xetera <mythologyali@gmail.com>
2019-07-30 20:47:44 -0700

    added client side syntax highlighting
    added client side syntax highlighting
    
    removed unnecessary packages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,5 @@
  */
 
 // You can delete this file if you're not using it
-// require("prismjs/themes/prism.css");
-// require("prismjs/themes/prism-tomorrow.css");
-// require("prismjs/plugins/line-numbers/prism-line-numbers.css");
+
+require('prismjs/themes/prism-tomorrow.css');
--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,5 +1,10 @@
 import React from "react"
 import * as SC from "./styles"
+import Prism from "prismjs"
+import "prismjs/components/prism-python"
+import "prismjs/components/prism-markup-templating"
+import "prismjs/components/prism-php"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 
 interface MarkdownProps {
   content: string
@@ -13,6 +18,9 @@ export function Markdown({
   content,
   ...restProps
 }: MarkdownProps): JSX.Element {
+  React.useEffect(() => {
+    Prism.highlightAll()
+  }, [])
   return (
     <SC.MarkdownWrapper
       dangerouslySetInnerHTML={{ __html: content }}
