bd0b202161464b98719ff964da9ce3c0c1d5ea86
Oden <hello@odensc.me>
2019-10-07 04:05:00 -0700

    Add href to DiscordButtonWrapper (fixes #117)

--- src/components/DiscordButton/index.tsx
+++ src/components/DiscordButton/index.tsx
@@ -3,7 +3,7 @@ import * as SC from "./styles"
 
 export const DiscordButton: FC = ({ children }) => {
   return (
-    <SC.DiscordButtonWrapper>
+    <SC.DiscordButtonWrapper href="https://discord.gg/programming">
       <SC.StyledDiscordLogo /> {children}
     </SC.DiscordButtonWrapper>
   )
