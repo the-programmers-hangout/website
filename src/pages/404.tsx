e4b2cd942d2e2c28b2234b2be378e21d1e158384
Åke Amcoff <ake.amcoff@lynx.agency>
2019-10-07 20:41:33 +0200

    Edit 404 page
    - New text (that is VERY informative)
    - Link that takes the user back to the main screen
    - Same styles as before

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react"
 
+import { Link } from "gatsby"
 import { SEO } from "../components/SEO"
 
 function NotFoundPage() {
   return (
     <Fragment>
       <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>404</h1>
+      <p>Are you lost?</p>
+      <Link to="/">Go back to safety</Link>
     </Fragment>
   )
 }
