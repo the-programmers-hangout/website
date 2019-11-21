36775d4ee8b2fa92123d4860a3992544f313f271
Jean-Philippe Sirois <jp@veksen.net>
2019-11-21 03:35:50 -0500

    style: lint codebase (dead import)

--- src/layouts/ColumnLayout/index.tsx
+++ src/layouts/ColumnLayout/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo, useState } from "react"
+import React, { FC, useState } from "react"
 
 import { HomeMobileHeader } from "../../components/HomeMobileHeader"
 import { GlobalStyles } from "../../globalStyles"
