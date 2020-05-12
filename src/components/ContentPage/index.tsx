ad3f172115e178e5d0a86a8871b0d5a1aafd5e6d
Jean-Philippe Sirois <jp@veksen.net>
2020-05-12 00:37:48 -0400

    refact: remove unused components

--- src/components/ContentPage/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import * as SC from "./styles"
-
-export const ContentPage: React.FC = ({ children, ...restProps }) => {
-  return (
-    <SC.ContentPageWrapper {...restProps}>{children}</SC.ContentPageWrapper>
-  )
-}
