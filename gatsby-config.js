9483ff98fd13ebeb2af61c0ff31f8e3dee310c2d
Jean-Philippe Sirois <jp@veksen.net>
2020-05-21 11:19:48 -0400

    perf: install gatsby-plugin-polished to optimize performance

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-polished`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
