104f3bdceaeb134b41747d093fbe14f59e9d2f6a
oof2win2 <honza.koco44@gmail.com>
2021-10-02 22:01:17 +0200

    :bug: Fix window not available, use sceeen height

--- src/components/HeaderBarebone/index.tsx
+++ src/components/HeaderBarebone/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { useDebounce, useWindowScroll } from "react-use"
+import { useDebounce, useWindowScroll, useWindowSize } from "react-use"
 import cx from "classnames"
 
 import * as SC from "./styles"
@@ -66,6 +66,7 @@ const StickyContainer: React.FC = ({ children, ...restProps }) => {
 export const HeaderBarebone = (props: IHeaderBareboneProps) => {
   const { y } = useWindowScroll()
   const [scrollY, setScrollY] = useState(0)
+  const {height} = useWindowSize()
 
   const [, cancel] = useDebounce(
     () => {
@@ -74,7 +75,6 @@ export const HeaderBarebone = (props: IHeaderBareboneProps) => {
     10,
     [y]
   )
-  const isMobile = window.navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i)
 
   useEffect(() => {
     return () => {
@@ -109,7 +109,7 @@ export const HeaderBarebone = (props: IHeaderBareboneProps) => {
           {!isBoxed && <SC.SingleTitle>{props.title}</SC.SingleTitle>}
         </Container>
       </SC.HeaderWrapper>
-      {scrollY >= 270 && !isMobile && stickyHeader}
+      {scrollY >= 270 && height >= 768 && stickyHeader}
     </>
   )
 }
